68e8d2400712aeef17f8ab52ce2f65d2ccafe86c
Ankit Tyagi <atyagi@credibility.com>
2014-04-11 16:20:03 -0700

    adding signature methods

--- src/Credibility/LaravelCybersource/Cybersource.php
+++ src/Credibility/LaravelCybersource/Cybersource.php
@@ -18,4 +18,23 @@ class Cybersource {
 
     }
 
+    public static function sign(array $params, $secretKey) {
+        return self::signData(self::buildDataToSign($params), $secretKey);
+    }
+
+    public static function signData($data, $secretKey) {
+        return base64_encode(hash_hmac('sha256', $data, $secretKey, true));
+    }
+
+    public static function buildDataToSign($params) {
+        $signedFieldNames = ArrayHelper::csvToArray($params["signed_field_names"]);
+        $signMe = '';
+        foreach ($signedFieldNames as $field) {
+            $signMe .= $field . '=' . $params[$field] . ',';
+        }
+
+        return rtrim($signMe, ',');
+    }
+
+
 } 
